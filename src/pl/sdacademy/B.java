67163641c9a9c6af6350c1160c7089ba5d2250db
dzarzycki <dar.zarzycki@gmail.com>
2017-04-10 15:32:15 +0200

    Dodanie klasy B

---
+++ src/pl/sdacademy/B.java
@@ -0,0 +1,20 @@
+package pl.sdacademy;
+
+public class B {
+	private int a;
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public B(int a) {
+		super();
+		this.a = a;
+	}
+	
+	
+}
